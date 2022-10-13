14335a4e56f261729d0a74f8e1e6948125f9c87e
mortadha1144 <mortadha114@gmail.com>
2022-10-13 12:27:09 +0300

    stack

--- bin/starter.dart
+++ bin/starter.dart
@@ -1,3 +1,20 @@
+import 'package:starter/stack.dart';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  final stack = Stack<int>();
+  stack.push(1);
+  stack.push(2);
+  stack.push(3);
+  stack.push(4);
+  print(stack);
+
+  final element = stack.pop();
+  print('Popped: $element');
+
+  const list = ['S', 'M', 'O', 'K', 'E'];
+
+  final smokeStack = Stack.of(list);
+  print(smokeStack);
+  smokeStack.pop();
+  print(smokeStack);
 }
